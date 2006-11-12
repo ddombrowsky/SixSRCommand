9c732a5f9a1c24d3989603c2047f3dfd52f56a21
davek <davek>
2006-11-12 06:29:05 +0000

    now can add checkboxes, but cant take them away

--- app/controllers/track_controller.rb
+++ app/controllers/track_controller.rb
@@ -1,5 +1,7 @@
 class TrackController < ApplicationController
-	scaffold :track
+
+# screw the worthless scaffolding crap
+#	scaffold :track
 
 	def list
 		@startrow = 0;
@@ -11,10 +13,47 @@ class TrackController < ApplicationController
 
 		#the list.rhtml file will not be parsed if this renderes text ???
 		#render_text @startrow;
-		
-		#@tracks = ["get","out"];
 	end
 
-	def edit
+
+	def commitchanges 
+
+		# DOES NOTHING
+		#t = Track.find(10);
+		#t["is_active"] = 1;
+
+		# calls is_active= method in Track, totall useless
+		#updates = { 10 => {"is_active",1}, 11=>{"is_active",1}};
+		#Track.update(updates.keys, updates.values);
+		
+		# calls is_active=, USELESS
+		#@t = Track.find(10);
+		#@t.is_active = 1;
+		#@t.save;
+
+		#@t = Track.find(10);
+		#@t.artist="YYYYYYYYYYYYXXXX";
+		#@t.save;
+		#render_text @t.track_id.to_s;
+		
+		# now this works.  problem was the Track class wasn't pointing
+		# to the correct database table
+		#@t = Track.find(10);
+		#@t.is_active=1;
+		#@t.save();
+
+		@params.each do |e|
+			if(e[0].to_i > 0) 
+				puts "updating record: #{e[0]} = #{e[1]}";
+				t = Track.find(e[0].to_i);
+				t.is_active=e[1].to_i;
+				t.save();
+			end
+		end
+
+		@sr = @params["startrow"];
+		puts "current startrow = #@sr";
+		redirect_to :action => "list", :params => {"startrow" => @sr};
 	end
 end
+
--- app/models/track.rb
+++ app/models/track.rb
@@ -1,7 +1,17 @@
 class Track < ActiveRecord::Base
+	set_table_name("list_data");
+	set_primary_key("id");
 
 	def Track.get_set(startrow)
-		@r = Track.find_by_sql "select * from tracks limit #{startrow},20";
+		@r = Track.find_by_sql "
+			select * 
+			from list_data
+			left join tracks using (track_id)
+			where list_id=1
+			limit #{startrow},20
+		";
 		return @r;
 	end
+
+	#attr_accessor :is_active;
 end
