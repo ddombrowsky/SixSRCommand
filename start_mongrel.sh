08c5b9f55534312dd49995803db8ed8668342699
David Dombrowsky <david.dombrowsky@redlion.net>
2013-10-24 16:11:10 -0400

    removed tdklint work

--- start_mongrel.sh
+++ start_mongrel.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
 set -x
-/var/lib/gems/1.8/bin/mongrel_rails start -p 3030 -a 192.168.1.2
+
+# mongrel is busted, grrrr...
+#/var/lib/gems/1.8/bin/mongrel_rails start -p 3030 -a 192.168.1.2
+
+./script/server -b 192.168.1.2 -p 3030
